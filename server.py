f8988a0433323417741b8093811f8fde1f37290f
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-06-13 22:52:06 -0700

    Made groudns on tet box feature

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, render_template
-from Oski import *
-import os
 app = Flask(__name__)
 
 answer_output = ''
@@ -9,10 +7,6 @@ answer_output = ''
 def hello():
 	return app.send_static_file('index.html')
 
-@app.route('/<path:path>')
-def static_proxy(path):
-    # send_static_file will guess the correct MIME type
-    return app.send_static_file(os.path.join('jcss', path))
 
 @app.route("/api/ReadTextInput", methods=["POST"])
 def receive_cheep():
@@ -23,24 +17,22 @@ def receive_cheep():
 def get_question():
 	transcribed_audio = request.get_json(force=True)
 	print(transcribed_audio)
-	found_answer = answer(request.get_json(force=True)['data'])
-	url_speech = text_to_voice_url(found_answer)
+	found_answer = "test"
+	url_speech = "test"
 	print(found_answer)
 	return render_template('answer.html', oski_answer=found_answer, speak=url_speech)
 
 	# = request.POST['data'].json()
 
-@app.route("/api/StartRecording", methods=["POST"])
-def receive_recording_cue():
-	print(request.form)
-	transcribed_audio = get_audio().capitalize()
-	while(transcribed_audio == ''):
-		transcribed_audio = get_audio.capitalize()
-	print(transcribed_audio)
-	found_answer = answer(transcribed_audio)
-	print(found_answer)
-	url_speech = text_to_voice_url(found_answer)
-	return render_template('oski_answer.html', name=transcribed_audio, oski_answer=found_answer, speak=url_speech)
+@app.route("/api/log_entry", methods=["POST"])
+def log_entry():
+	print ("Kush")
+	print(request.form['dream_content'])
+	# print (request.get_json(force=True))
+	return app.send_static_file('index.html')
+	#return render_template('answer.html', oski_answer=found_answer, speak=url_speech)
+
+	# = request.POST['data'].json()
 
 if __name__ == "__main__":
 	app.run(debug=True)
