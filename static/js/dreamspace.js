a9630227de611fe4b13e5f43e60a55c789dcf012
Kurush Dubash <dubash.kurush@berkeley.edu>
2016-07-17 19:22:37 -0700

    Got sign ups to work

--- static/js/dreamspace.js
+++ static/js/dreamspace.js
@@ -4,5 +4,6 @@ app.config(function($routeProvider) {
     .when("/", {templateUrl : "partials/login.html"})
     .when("/login", {templateUrl : "partials/login.html"})
     .when("/about", {templateUrl : "partials/about.html"})
+    .when("/dashboard", {templateUrl : "partials/dashboard.html"})
     .when("/signup", {templateUrl : "partials/signup.html"});
 });
